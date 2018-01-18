ef63f3c24d8a31b7ed6d51708d5c4ab1668bba89
Eckarath <eckastudio@gmail.com>
2018-01-18 11:21:28 +0100

    Make test failed

--- src/main/java/io/github/joaomlneto/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/io/github/joaomlneto/travis_ci_tutorial_java/SimpleCalculator.java
@@ -1,23 +1,27 @@
 package io.github.joaomlneto.travis_ci_tutorial_java;
 
 public class SimpleCalculator {
+	
 	public int add(int a, int b) {
 		return a + b;
 	}
+
 	public int sub(int a, int b) {
 		return a - b;
 	}
+
 	public int mul(int a, int b) {
 		return a * b;
 	}
+	
 	public int div(int a, int b) {
 		return a / b;
 	}
+
 	public static final void main(String[] args) {
 		System.out.println("Hello world?");
 
 		SimpleCalculator calculator = new SimpleCalculator();
-
 		System.out.println("Addition: " + calculator.add(12, 10));
 	}
 }
